c675a40551a384c2d464a73519fdf85a0edc2fbe
Santiago Monsalve Orrego <smonsalve@sistecredito.com>
2023-01-19 15:46:55 -0500

    Primera entrega / primera misión

--- src/app/pages/quotes-show/quotes-show.component.ts
+++ src/app/pages/quotes-show/quotes-show.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 import { Quote } from 'src/app/shared/models/quote.model';
 import { InformationService } from 'src/app/shared/services/information.service';
 
@@ -10,23 +9,31 @@ import { InformationService } from 'src/app/shared/services/information.service'
 })
 export class QuotesShowComponent {
 
-  quoteData: Quote
+  quoteData: Quote;
+  quotesArray: Quote[] = [];
+  // quotesArrayDos: Array<Quote>; 
 
   constructor(private informationService: InformationService) { 
+    this.getQuote();
+  }
+
+  getQuote() {
 
     this.informationService.getAnimequotes().subscribe(data => {
-      this.quoteData = data;
+      this.quotesArray.push(data);
+      this.quoteData = this.quotesArray[0];
+      console.log(this.quotesArray);
+
+      
     });
-
-
-    
   }
 
   deleteQuotes(){
-
+    
     console.log("nada");
     
-      }
+      
+  }
      
   
 
